394ddf143c9eadd41e279beef27e8e90d428df74
github961129 <zzcheng9611@163.com>
2021-08-30 22:47:51 +0800

    删除了5.wbt

--- go/build/release/go.d
+++
@@ -1,5 +0,0 @@
-build/release/go.o: go.c \
- D:\Program\ Files\ (x86)\Webots/include/controller/c/webots/robot.h \
- D:\Program\ Files\ (x86)\Webots/include/controller/c/webots/types.h \
- D:\Program\ Files\ (x86)\Webots/include/controller/c/webots/nodes.h \
- D:\Program\ Files\ (x86)\Webots/include/controller/c/webots/motor.h
